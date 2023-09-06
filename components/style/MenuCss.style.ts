f92888c3c323c3cdebfe8d76c49752f9b4617f48
diposs <onakunle.dipo@gmail.com>
2023-09-06 10:34:05 +0100

    Update MenuCss.style.ts

--- components/style/MenuCss.style.ts
+++ components/style/MenuCss.style.ts
@@ -3,10 +3,28 @@ import { createStyles } from '@mantine/core';
 
 export default createStyles ((theme) => ({
     links: {
-        width: '50dvw',
-        [theme.fn.smallerThan('sm')]: {
-            display: 'none',
-        },
-      },
-    })
-    )
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  link: {
+    display: 'block',
+    lineHeight: 1,
+    padding: '8px 12px',
+    borderRadius: theme.radius.sm,
+    textDecoration: 'none',
+    color: theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    '&:hover': {
+      backgroundColor:theme.colors.gray[0],
+    },
+  },
+
+  linkLabel: {
+    marginRight: '5px',
+  },
+})
+)
