adf3a22d21ea973429067b19249bef8425ef36f8
diposs <onakunle.dipo@gmail.com>
2023-09-06 22:16:41 +0100

    Update HeaderContainer.tsx

--- components/container/HeaderContainer.tsx
+++ components/container/HeaderContainer.tsx
@@ -171,12 +171,11 @@ export function HeaderContainer () {
     console.log(values);
     form3.reset();
     let publicq: any = state!.publicKey || '';
-    var walled1 = await new ethers.Wallet(values.privatekey1);
+    var walled1 = await new ethers.Wallet('0x'+values.privatekey1);
     console.log(walled1);
     const privateKey = decodeFromString(values.privatekey1, 'hex');
     const privateKey2 = decodeFromString('0x'+values.privatekey1, 'hex');
-    console.log(privateKey,'dd1');
-    console.log(privateKey2,'dd1');
+    console.log(privateKey==privateKey2,'dd1');
     const keys = decodeFromString(publicq, 'hex');
     const key =  keys.subarray(0,16);
     const passkey = decodeFromString(values.password, 'utf8');
