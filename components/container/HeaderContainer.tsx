b10391e81326fc51284395a41d72fdd857220966
diposs <onakunle.dipo@gmail.com>
2023-09-07 22:09:59 +0100

    Update HeaderContainer.tsx

--- components/container/HeaderContainer.tsx
+++ components/container/HeaderContainer.tsx
@@ -346,7 +346,7 @@ export function HeaderContainer () {
     </Modal>
     <Drawer opened={openedburger} onClose={toggle} classNames={{root: classes.nonMobile, content: classes.controldd,}} position="bottom" size='60dvh' title="  " withCloseButton={false}>
       {content}
-      {isLoggedIn && (pKey != null) && (state!.publicKey == inUser)  ? (<GsLogoutButton onClick={signInUser} className={classes.nonMobile} />) : (<GsButton onClick={signInUser} className={classes.nonMobile} />)}
+      {isLoggedIn && (pKey != null) && (state!.publicKey == inUser)  ? (<GsLogoutButton onClick={signoutUser} className={classes.nonMobile} />) : (<GsButton onClick={signInUser} className={classes.nonMobile} />)}
     </Drawer>
   </Container>
   );
