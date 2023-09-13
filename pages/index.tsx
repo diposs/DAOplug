37d0393ceae5e4fc83be2aa76c8749647d6c2c89
diposs <onakunle.dipo@gmail.com>
2023-09-13 16:23:43 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,6 +85,236 @@ export default function Home() {
         Select files
       </Button>
     </div>
+      <div className={classes.wrapper}>
+      <Dropzone
+        openRef={openRef}
+        onDrop={() => {}}
+        className={classes.dropzone}
+        radius="md"
+        accept={[MIME_TYPES.pdf]}
+        maxSize={30 * 1024 ** 2}
+      >
+        <div style={{ pointerEvents: 'none' }}>
+          <Group position="center">
+            <Dropzone.Accept>
+              <IconDownload
+                size={rem(50)}
+                color={theme.colors[theme.primaryColor][6]}
+                stroke={1.5}
+              />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <IconX size={rem(50)} color={theme.colors.red[6]} stroke={1.5} />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <IconCloudUpload
+                size={rem(50)}
+                color={theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black}
+                stroke={1.5}
+              />
+            </Dropzone.Idle>
+          </Group>
+
+          <Text ta="center" fw={700} fz="lg" mt="xl">
+            <Dropzone.Accept>Drop files here</Dropzone.Accept>
+            <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
+            <Dropzone.Idle>Upload resume</Dropzone.Idle>
+          </Text>
+          <Text ta="center" fz="sm" mt="xs" c="dimmed">
+            Drag&apos;n&apos;drop files here to upload. We can accept only <i>.pdf</i> files that
+            are less than 30mb in size.
+          </Text>
+        </div>
+      </Dropzone>
+
+      <Button className={classes.control} size="md" radius="xl" onClick={() => openRef.current?.()}>
+        Select files
+      </Button>
+    </div>
+    <div className={classes.wrapper}>
+      <Dropzone
+        openRef={openRef}
+        onDrop={() => {}}
+        className={classes.dropzone}
+        radius="md"
+        accept={[MIME_TYPES.pdf]}
+        maxSize={30 * 1024 ** 2}
+      >
+        <div style={{ pointerEvents: 'none' }}>
+          <Group position="center">
+            <Dropzone.Accept>
+              <IconDownload
+                size={rem(50)}
+                color={theme.colors[theme.primaryColor][6]}
+                stroke={1.5}
+              />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <IconX size={rem(50)} color={theme.colors.red[6]} stroke={1.5} />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <IconCloudUpload
+                size={rem(50)}
+                color={theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black}
+                stroke={1.5}
+              />
+            </Dropzone.Idle>
+          </Group>
+
+          <Text ta="center" fw={700} fz="lg" mt="xl">
+            <Dropzone.Accept>Drop files here</Dropzone.Accept>
+            <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
+            <Dropzone.Idle>Upload resume</Dropzone.Idle>
+          </Text>
+          <Text ta="center" fz="sm" mt="xs" c="dimmed">
+            Drag&apos;n&apos;drop files here to upload. We can accept only <i>.pdf</i> files that
+            are less than 30mb in size.
+          </Text>
+        </div>
+      </Dropzone>
+
+      <Button className={classes.control} size="md" radius="xl" onClick={() => openRef.current?.()}>
+        Select files
+      </Button>
+    </div>
+<div className={classes.wrapper}>
+      <Dropzone
+        openRef={openRef}
+        onDrop={() => {}}
+        className={classes.dropzone}
+        radius="md"
+        accept={[MIME_TYPES.pdf]}
+        maxSize={30 * 1024 ** 2}
+      >
+        <div style={{ pointerEvents: 'none' }}>
+          <Group position="center">
+            <Dropzone.Accept>
+              <IconDownload
+                size={rem(50)}
+                color={theme.colors[theme.primaryColor][6]}
+                stroke={1.5}
+              />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <IconX size={rem(50)} color={theme.colors.red[6]} stroke={1.5} />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <IconCloudUpload
+                size={rem(50)}
+                color={theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black}
+                stroke={1.5}
+              />
+            </Dropzone.Idle>
+          </Group>
+
+          <Text ta="center" fw={700} fz="lg" mt="xl">
+            <Dropzone.Accept>Drop files here</Dropzone.Accept>
+            <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
+            <Dropzone.Idle>Upload resume</Dropzone.Idle>
+          </Text>
+          <Text ta="center" fz="sm" mt="xs" c="dimmed">
+            Drag&apos;n&apos;drop files here to upload. We can accept only <i>.pdf</i> files that
+            are less than 30mb in size.
+          </Text>
+        </div>
+      </Dropzone>
+
+      <Button className={classes.control} size="md" radius="xl" onClick={() => openRef.current?.()}>
+        Select files
+      </Button>
+    </div>
+      <div className={classes.wrapper}>
+      <Dropzone
+        openRef={openRef}
+        onDrop={() => {}}
+        className={classes.dropzone}
+        radius="md"
+        accept={[MIME_TYPES.pdf]}
+        maxSize={30 * 1024 ** 2}
+      >
+        <div style={{ pointerEvents: 'none' }}>
+          <Group position="center">
+            <Dropzone.Accept>
+              <IconDownload
+                size={rem(50)}
+                color={theme.colors[theme.primaryColor][6]}
+                stroke={1.5}
+              />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <IconX size={rem(50)} color={theme.colors.red[6]} stroke={1.5} />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <IconCloudUpload
+                size={rem(50)}
+                color={theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black}
+                stroke={1.5}
+              />
+            </Dropzone.Idle>
+          </Group>
+
+          <Text ta="center" fw={700} fz="lg" mt="xl">
+            <Dropzone.Accept>Drop files here</Dropzone.Accept>
+            <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
+            <Dropzone.Idle>Upload resume</Dropzone.Idle>
+          </Text>
+          <Text ta="center" fz="sm" mt="xs" c="dimmed">
+            Drag&apos;n&apos;drop files here to upload. We can accept only <i>.pdf</i> files that
+            are less than 30mb in size.
+          </Text>
+        </div>
+      </Dropzone>
+
+      <Button className={classes.control} size="md" radius="xl" onClick={() => openRef.current?.()}>
+        Select files
+      </Button>
+    </div>
+      <div className={classes.wrapper}>
+      <Dropzone
+        openRef={openRef}
+        onDrop={() => {}}
+        className={classes.dropzone}
+        radius="md"
+        accept={[MIME_TYPES.pdf]}
+        maxSize={30 * 1024 ** 2}
+      >
+        <div style={{ pointerEvents: 'none' }}>
+          <Group position="center">
+            <Dropzone.Accept>
+              <IconDownload
+                size={rem(50)}
+                color={theme.colors[theme.primaryColor][6]}
+                stroke={1.5}
+              />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <IconX size={rem(50)} color={theme.colors.red[6]} stroke={1.5} />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <IconCloudUpload
+                size={rem(50)}
+                color={theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black}
+                stroke={1.5}
+              />
+            </Dropzone.Idle>
+          </Group>
+
+          <Text ta="center" fw={700} fz="lg" mt="xl">
+            <Dropzone.Accept>Drop files here</Dropzone.Accept>
+            <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
+            <Dropzone.Idle>Upload resume</Dropzone.Idle>
+          </Text>
+          <Text ta="center" fz="sm" mt="xs" c="dimmed">
+            Drag&apos;n&apos;drop files here to upload. We can accept only <i>.pdf</i> files that
+            are less than 30mb in size.
+          </Text>
+        </div>
+      </Dropzone>
+
+      <Button className={classes.control} size="md" radius="xl" onClick={() => openRef.current?.()}>
+        Select files
+      </Button>
+    </div>
       ) : (
       <></>
     )}
