034fd779d0f03dc3f19ca5c786bf9a7527221cff
diposs <onakunle.dipo@gmail.com>
2023-09-15 12:36:59 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,21 @@
 import Document, { DocumentContext } from 'next/document';
 import { ServerStyles, createStylesServer } from '@mantine/next';
+import { rtlCache } from '../rtl-cache';
 
-// optional: you can provide your cache as a first argument in createStylesServer function
-const stylesServer = createStylesServer();
+const stylesServer = createStylesServer(rtlCache);
 
 export default class _Document extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
 
-    // Add your app specific logic here
-
     return {
       ...initialProps,
-      styles: [
-        initialProps.styles,
-        <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />,
-      ],
+      styles: (
+        <>
+          {initialProps.styles}
+          <ServerStyles html={initialProps.html} server={stylesServer} />
+        </>
+      ),
     };
   }
 }
